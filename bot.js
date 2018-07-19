7454208d0757b5e5bba221871e58ebd8bbf95ce8
Helloxddd <37207067+Helloxddd@users.noreply.github.com>
2018-07-19 23:55:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ client.on('ready', () => {
 });
 
 client.on('ready', () => {
-client.user.setActivity("WE NEED TO MAKE THE BOT IN 20 SERVER I M IN ${client.guilds.size}" ,{type: "WATCHING"});
+client.user.setActivity(`WE NEED TO MAKE THE BOT IN 20 SERVER I M IN "${client.guilds.size}"` ,{type: "WATCHING"});
 });
 
 client.on('message', msg => {
