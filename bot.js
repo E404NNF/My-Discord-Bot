3742114deedc7511cfc89249c10d613f5b675b58
Helloxddd <37207067+Helloxddd@users.noreply.github.com>
2018-07-20 01:39:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -971,7 +971,7 @@ client.on('message', message => {
               message.channel.send(`⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**`);
             console.log(`⚠️ جاري اعادة تشغيل البوت... ⚠️`);
             client.destroy();
-            child_process.fork(__dirname + "/myerrorbot404.js");
+            child_process.fork(__dirname + "/bot.js");
             console.log(`تم اعادة تشغيل البوت`);
         }
       
