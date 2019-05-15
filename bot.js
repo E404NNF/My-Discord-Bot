92c7ee538d700fc92ccd96a22fe0cd50ce2caed9
Helloxddd <37207067+Helloxddd@users.noreply.github.com>
2019-05-16 01:08:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,10 @@ client.on('ready', () => {
   console.log('')
   console.log('╚[════════════════════════════════════]╝')
 });
-
+client.on('error', data => {
+  console.log('error',data);
+  // attempt reconnection x times, after x seconds, exponential backoff
+});
 client.on('ready', function(){
     client.user.setStatus("dnd");
     var ms = 100000 ;
@@ -278,13 +281,65 @@ client.on('message', message => {
         message.channel.sendFile("http://thecatapi.com/api/images/get?format=src&type=png", "cat.png")
     }
 });
-      
+const DOG_API_URL   = "https://api.thedogapi.com/"
+const DOG_API_KEY   = "b9217e7c-c03c-4dde-9c54-7faa0578be18"; // get a free key from - https://thedogapi.com/signup
 client.on('message', message => {
     if (message.content === 'error$dog') {
 
         message.channel.sendFile("http://thedogapi.com/api/images/get?format=src&type=png", "dog.png")
     }
 });
+
+async function messageRecieved(message)
+{
+  try{
+    // pass the name of the user who sent the message for stats later, expect an array of images to be returned.
+    var images = await loadImage(message.author.username);
+
+    // get the Image, and first Breed from the returned object.
+    var image = images[0];
+    var breed = image.breeds[0];
+
+    console.log('message processed','showing',breed)
+    // use the *** to make text bold, and * to make italic
+    message.channel.send( "***"+breed.name + "*** \r *"+breed.temperament+"*", { files: [ image.url ] } );
+    // if you didn't want to see the text, just send the file
+
+  }catch(error)
+  {
+    console.log(error)
+  }
+}
+/**
+ * Makes a request to theDogAPI.com for a random dog image with breed info attached.
+ */
+async function loadImage(sub_id)
+{
+  // you need an API key to get access to all the iamges, or see the requests you've made in the stats for your account
+  var headers = {
+      'X-API-KEY': DOG_API_KEY,
+  }
+  var query_params = {
+    'has_breeds':true, // we only want images with at least one breed data object - name, temperament etc
+    'mime_types':'jpg,png', // we only want static images as Discord doesn't like gifs
+    'size':'small',   // get the small images as the size is prefect for Discord's 390x256 limit
+    'sub_id': sub_id, // pass the message senders username so you can see how many images each user has asked for in the stats
+    'limit' : 1       // only need one
+  }
+  // convert this obejc to query string 
+  let queryString = querystring.stringify(query_params);
+
+  try {
+    // construct the API Get request url
+    let _url = DOG_API_URL + `v1/images/search?${queryString}`;
+    // make the request passing the url, and headers object which contains the API_KEY
+    var response = await r2.get(_url , {headers} ).json
+  } catch (e) {
+      console.log(e)
+  }
+  return response;
+
+}
 client.on('message', function(msg) {
     if(msg.content.startsWith (prefix  + 'serverinfo')) {
       let embed = new Discord.RichEmbed()
