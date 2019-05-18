556d1a8310dc1ad8733bdc3dbce1db8b060d6b10
Helloxddd <37207067+Helloxddd@users.noreply.github.com>
2019-05-18 02:56:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1098,7 +1098,7 @@ client.on("message", message => {
 
     if (message.content.startsWith(prefix + "tierlist")) {
         let q
-        rea = message.content.substring(9)
+        rea = message.content.substring(10)
         re = /\|/i
         if (re.test(rea)==false) {
             message.channel.send("Seperate your inputs with `|`s")
